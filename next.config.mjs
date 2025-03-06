78cf44f055ef81e6ba9b28549cb6b276690d6442
Maneesh Kurumaddali <maneeshkurumaddali@Maneesh-BK.local>
2025-03-06 13:09:44 +0530

    changes1

--- next.config.mjs
+++ next.config.mjs
@@ -21,6 +21,8 @@ const nextConfig = {
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
   },
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/to-do-app' : '',
 }
 
 mergeConfig(nextConfig, userConfig)
